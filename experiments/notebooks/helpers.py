e49bc08f49d670afa774b79737e92ab9541d45fc
Fabio Tomaz <fmts@ua.pt>
2020-04-21 07:11:14 -0400

    Ensemble study added

--- experiments/notebooks/helpers.py
+++ experiments/notebooks/helpers.py
@@ -8,11 +8,10 @@ from data.data_loader import load_isic_training_data
 from collections import Counter
 
 
-def read_models_info(history_folder_name, pred_val_folder_name, pred_test_folder_name):
+def read_models_info(history_folder_name, pred_test_folder_name):
     models_info = []
 
     history_folder=os.path.join("..", history_folder_name)
-    pred_val_folder=os.path.join("..", pred_val_folder_name)
     pred_test_folder=os.path.join("..", pred_test_folder_name)
 
     model_names = [o for o in os.listdir(history_folder) if os.path.isdir(os.path.join(history_folder,o))]
@@ -23,7 +22,6 @@ def read_models_info(history_folder_name, pred_val_folder_name, pred_test_folder
         for i, combination in enumerate(hyperparameter_combinations): 
             hyperparameters = get_hyperparameters_from_str(combination)
             file_path = os.path.join(history_folder, model_name, combination, "0", "training.csv")
-            pred_val_path = os.path.join(pred_val_folder, model_name, combination)
             pred_test_path = os.path.join(pred_test_folder, model_name, combination)
 
             if len(hyperparameters)>1 and os.path.exists(file_path):
@@ -31,7 +29,6 @@ def read_models_info(history_folder_name, pred_val_folder_name, pred_test_folder
                     "model": model_name,
                     "hyperparameters": hyperparameters, 
                     "log": file_path,
-                    "pred_val": pred_val_path if os.path.exists(pred_val_path) else None,
                     "pred_test": pred_test_path if os.path.exists(pred_test_path) else None
                 }
                 models_info.append(model_info)
@@ -64,29 +61,28 @@ def filter_models_info(models_info, models=None, parameters=None):
 
 
 def read_ensembles(
-    pred_test_folder_name,
+    pred_test_folder,
     contains_models=None
 ):
     models_info = []
 
-    pred_test_folder=os.path.join("..", pred_test_folder_name)
+    ensemble_names = [o for o in os.listdir(pred_test_folder) if os.path.isdir(os.path.join(pred_test_folder,o)) and "-" in o]
+    for ensemble_name in ensemble_names:
+        pred_test_path=os.path.join(pred_test_folder, ensemble_name)
+        models=ensemble_name.split("-")
+        if contains_models is not None:
+            add=True
+            for model_to_contain in contains_models:
+                if model_to_contain not in models:
+                    add=False
+            if add is False:
+                continue 
 
-    model_names = [o for o in os.listdir(pred_test_folder) if os.path.isdir(os.path.join(pred_test_folder,o))]
-    for model_name in model_names:
-        hyperparameter_combinations = [o for o in os.listdir(log_dir) if os.path.isdir(os.path.join(log_dir,o))]
-
-        for i, combination in enumerate(hyperparameter_combinations): 
-            hyperparameters = get_hyperparameters_from_str(combination)
-            file_path = os.path.join(history_folder, model_name, combination, "0", "training.csv")
-            pred_val_path = os.path.join(pred_val_folder, model_name, combination)
-            pred_test_path = os.path.join(pred_test_folder, model_name, combination)
-
-            if len(hyperparameters)>1 and os.path.exists(file_path):
-                model_info = {
-                    "models": model_name,
-                    "pred_test": pred_test_path if os.path.exists(pred_test_path) else None
-                }
-                models_info.append(model_info)
+        model_info = {
+            "models": ensemble_name.split("-"),
+            "pred_test": pred_test_path,
+        }
+        models_info.append(model_info)
     
     return models_info
 
